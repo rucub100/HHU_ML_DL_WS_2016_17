6cbd89c85d6bd6c5fd010fbd368e6992e5ab2262
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-11-20 18:43:13 +0100

    Added two pooling layers --> improve accuracy

--- Project 1/cnn.py
+++ Project 1/cnn.py
@@ -44,6 +44,9 @@ def conv2d(x, W):
     """Use a conv layer with weights W on zero padded input x and stride 1"""
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
 
 def train_neural_network(x):
     # x is 1x28*28 ( x = [34,43,5,6,0,0,7,5,...] )
@@ -55,17 +58,20 @@ def train_neural_network(x):
     x_image = tf.reshape(x, [-1, 28, 28, 1])
     # h_conv1 is result of applied conv filter W to x
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
 
     # Apply second convolutional network
     W_conv2 = weight_variable([5, 5, 32, 16])
     b_conv2 = bias_variable([16])
-    h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    
+    h_pool2 = max_pool_2x2(h_conv2)
 
     #Fully connected layer
-    W_fc1 = weight_variable([28 * 28 * 16, 50])
+    W_fc1 = weight_variable([7 * 7 * 16, 50])
     b_fc1 = bias_variable([50])
-    h_conv1_flat = tf.reshape(h_conv2, [-1, 28*28*16])
-    h_fc1 = tf.nn.relu(tf.matmul(h_conv1_flat, W_fc1) + b_fc1)
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*16])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     keep_prob = tf.placeholder(tf.float32)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
