e3960358af092b1a96790d03f5c981cd443f709b
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-12-06 20:10:45 +0100

    Add config.json and CNN code

--- Project 1/cnn.py
+++ Project 1/cnn.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
+import json
+import sys
 
 import tensorflow as tf
 
@@ -12,9 +14,16 @@ from tensorflow.examples.tutorials.mnist import input_data
 Define layer size etc
 """
 
-LEARNING_RATE = 0.01
+config = None
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+if config == None:
+    print "Config file 'config.json' not found"
+    sys.exit(1)
+
+LEARNING_RATE = config['learning_rate']
 #use batches to shiffer through reasonable data patches
-BATCH_SIZE = 100
+BATCH_SIZE = config['batch_size']
 #784=28*28 pixels in all mnist pics
 MNIST_WIDTH = 28
 MNIST_HEIGHT = 28
@@ -49,28 +58,31 @@ def max_pool_2x2(x):
 
 
 def train_neural_network(x):
-    # x is 1x28*28 ( x = [34,43,5,6,0,0,7,5,...] )
-    # W_conv1 is 5x5
-    # b_conv1 is 1x32
-    W_conv1 = weight_variable([7,7,1,32])
-    b_conv1 = bias_variable([32])
     # input data is embedded in 4d tensor
+    current_size = 28
+    current_depth = 1
     x_image = tf.reshape(x, [-1, 28, 28, 1])
-    # h_conv1 is result of applied conv filter W to x
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
-
-    # Apply second convolutional network
-    W_conv2 = weight_variable([5, 5, 32, 16])
-    b_conv2 = bias_variable([16])
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    
-    h_pool2 = max_pool_2x2(h_conv2)
+    # x is 1x28*28 ( x = [34,43,5,6,0,0,7,5,...] )
+    for layer in config['layers']:
+        type = layer['type']
+        if type == "convolution":
+            width = layer['size']
+            height = width
+            depth = layer['depth']
+            w_conv = weight_variable([width, height, current_depth, depth])
+            b_conv = bias_variable([depth])
+            x_image = tf.nn.relu(conv2d(x_image, w_conv) + b_conv)
+            current_depth = depth
+        elif type == "pool":
+            x_image = max_pool_2x2(x_image)
+            current_size = current_size / 2
 
     #Fully connected layer
-    W_fc1 = weight_variable([7 * 7 * 16, 50])
+    print "current_size:",current_size
+    print "current_depth:",current_depth
+    W_fc1 = weight_variable([int(current_size * current_size * current_depth), 50])
     b_fc1 = bias_variable([50])
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*16])
+    h_pool2_flat = tf.reshape(x_image, [-1, int(current_size * current_size * current_depth)])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     keep_prob = tf.placeholder(tf.float32)
@@ -81,14 +93,16 @@ def train_neural_network(x):
 
     y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
+    training_rounds = config['training_rounds']
+    training_round_size = config['training_round_size']
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
     train_step = tf.train.AdamOptimizer(0.01).minimize(cross_entropy)
     correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     sess.run(tf.initialize_all_variables())
-    for i in range(20000):
+    for i in range(training_rounds * training_round_size):
         batch = mnist.train.next_batch(BATCH_SIZE)
-        if i%BATCH_SIZE == 0:
+        if i%training_round_size == 0:
             train_accuracy = accuracy.eval(feed_dict={
                     x:batch[0], y_: batch[1], keep_prob: 1.0})
             print("step %d, training accuracy %f"%(i, train_accuracy))
